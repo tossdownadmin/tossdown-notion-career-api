e4cbeaa75ad2adbd3c352a0e6031d3a0c7e61bc0
shery03 <sheharyarkhalid03@gmail.com>
2025-11-25 17:21:01 +0500

    remove applicaiton count fun test

--- routes/applications.js
+++ routes/applications.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const applicationController = require("../controllers/applicationController");
 
+
 // GET /api/applications (with optional id query parameter)
 router.get('/', applicationController.getApplications);
 
